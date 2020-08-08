f344de2dc38fd9b18b3c71c31afa078c273360b5
Alexis Tan <alexis@sen-dev.com>
2020-08-08 16:42:41 +0200

    Add support for more randomly picked responses

--- bot.mjs
+++ bot.mjs
@@ -2,13 +2,25 @@
 import Discord from 'discord.js-light'
 const client = new Discord.Client()
 
+const generateReply = () => {
+  let randomNumber = Math.floor(Math.random() * 1000)
+
+  if (randomNumber == 0) {
+    return 'False!'
+  } else if (randomNumber < 10) {
+    return 'Maybe?'
+  } else {
+    return 'True!'
+  }
+}
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on('message', message => {
   if (message.mentions.users.has(client.user.id)) {
-    message.reply('True!');
+    message.reply(generateReply());
   }
 })
 
